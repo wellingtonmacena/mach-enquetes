e09f7b250d020aff6f70e50f05791c2989489bb6
wellingtonmacena <wellington.macena.23@gmail.com>
2023-03-05 17:21:12 -0300

    added mobile project

---
+++ BACKEND/MachEnquetes/Models/Survey.cs
@@ -0,0 +1,81 @@
+﻿using MachEnquetes.Application;
+
+namespace MachEnquetes.Models
+{
+    public class Survey
+    {
+        [SwaggerExclude]
+        public string Id
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <example>What is your favorite color?</example>
+        public string Title
+        {
+            get => default;
+            set
+            {
+            }
+        }
+        /// <example>["red", "blue", "yellow"]</example>
+        public List<VoteOption> VoteOptions
+        {
+            get => default;
+            set
+            {
+            }
+        }
+        /// <example>No</example>
+        public bool CanUnregistredVote
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public List<Vote> Votes
+        {
+            get => default;
+            set
+            {
+            }
+        }
+        /// <example>2</example>
+        public int QuantityOfOptionsCanBeSelected
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <example>"2017-09-08T19:01:55.714942+03:00"</example>
+        public DateTime? LastModifiedDate { get; set; }
+        /// <example>"2017-09-08T19:01:55.714942+03:00"</example>
+        public DateTime? CreatedDate { get; set; }
+        /// <example>"2017-09-08T19:01:55.714942+03:00"</example>
+        public DateTime FinishDate
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+
+        public Survey()
+        {
+
+        }
+
+        public Survey(string id) : this()
+        {
+            Id = id;
+        }
+    }
+}
---
+++ BACKEND/MachEnquetes/Utils/DatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace MachEnquetes.Utils
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string DatabaseNameHomol { get; set; }
+        public string NoteCollectionName { get; set; }
+        public string UserCollectionName { get; set; }
+        public string SurveyCollectionName { get; set; }
+    }
+}
